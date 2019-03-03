961af1d8810e5057758b39925d09d6a8983dacc4
Unknown <slyguggi@gmail.com>
2019-03-03 10:14:57 +0100

    fixed blood splats bugs

--- Assets/Scripts/BloodSplats.cs
+++ Assets/Scripts/BloodSplats.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class BloodSplats : MonoBehaviour
 {
-	[SerializeField] private int minAnimID = 0;
+	[SerializeField] private int minAnimID = 1;
 	[SerializeField] private int maxAnimID = 3;
+	[SerializeField] private AudioClip[] splatsSounds = null;
 	private bool isAlive = true;
 
 	private void Start()
 	{
-		GetComponent<Animator>().SetTrigger(Mathf.RoundToInt(Random.Range(minAnimID, maxAnimID)));
+		GetComponent<AudioSource>().PlayOneShot(splatsSounds[Random.Range(0, splatsSounds.Length)]);
+		GetComponent<Animator>().SetTrigger(Mathf.RoundToInt(Random.Range(minAnimID, maxAnimID)).ToString());
 	}
 
 	public void Destroy()
