e3fc450094e2fb89b7e7e0fc8033abd62b2a4b59
Nicolas <esplemea@gmail.com>
2019-03-03 12:25:12 +0100

    hands behaviour new system

--- Assets/Scripts/DirectionTrigger.cs
+++ Assets/Scripts/DirectionTrigger.cs
@@ -7,8 +7,10 @@ public class DirectionTrigger : MonoBehaviour
 {
 	[SerializeField] private CardinalDirectionTrigger directionTrigger;
 	[SerializeField] private LevelSpawnTrack spawner = null;
+    [SerializeField] private HandsBehaviour handLeft = null;
+    [SerializeField] private HandsBehaviour handRight = null;
 
-	private CinemachineVirtualCamera _globalCam;
+    private CinemachineVirtualCamera _globalCam;
 	private CinemachineBasicMultiChannelPerlin _noise;
 
 	public enum CardinalDirectionTrigger
@@ -21,17 +23,45 @@ public class DirectionTrigger : MonoBehaviour
 		Left
 	}
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.CompareTag("Player"))
-		{
-			foreach (var ennemy in spawner.EnnemiesAlive)
-			{
-				if (ennemy.IsHitable())
-				{
-					ennemy.OnHitByPlayer(directionTrigger);
-				}
-			}
-		}
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            Vector3 speed = other.gameObject.GetComponent<HandsBehaviour>().currentSpeed;
+            Debug.Log("Speed " + speed + " type " + directionTrigger);
+            if (speed.magnitude > 10)
+            {
+                bool correct = false;
+                switch (directionTrigger)
+                {
+                    case DirectionTrigger.CardinalDirectionTrigger.TopRight:
+                    case DirectionTrigger.CardinalDirectionTrigger.TopLeft:
+                        correct = speed.z > Mathf.Abs(speed.x);
+                        break;
+                    case DirectionTrigger.CardinalDirectionTrigger.BottomRight:
+                    case DirectionTrigger.CardinalDirectionTrigger.BottomLeft:
+                        correct = -speed.z > Mathf.Abs(speed.x);
+                        break;
+                    case DirectionTrigger.CardinalDirectionTrigger.Right:
+                        correct = speed.x > Mathf.Abs(speed.z);
+                        break;
+                    case DirectionTrigger.CardinalDirectionTrigger.Left:
+                        correct = -speed.x > Mathf.Abs(speed.z);
+                        break;
+                }
+                if (correct)
+                {
+                    foreach (var ennemy in spawner.EnnemiesAlive)
+                    {
+                        if (ennemy.IsHitable())
+                        {
+                            ennemy.OnHitByPlayer(directionTrigger);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
 }
---
+++ Assets/Scripts/HandsBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsBehaviour : MonoBehaviour
+{
+    private Vector3 lastPostion;
+    public Vector3 currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPostion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpeed = lastPostion - transform.position / Time.deltaTime;
+        lastPostion = transform.position;
+    }
+}
