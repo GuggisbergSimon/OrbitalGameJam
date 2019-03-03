5b930f0d20cd6a0209c2ed3421c33e6f12240a83
Unknown <slyguggi@gmail.com>
2019-03-03 12:28:44 +0100

    added whooses sounds

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Minion : MonoBehaviour
 {
+	[SerializeField] private AudioClip[] breakingNoises = null;
 	private Animator _myAnimator;
-	
+	private AudioSource _myAudioSource;
+
 	private void Start()
 	{
 		_myAnimator = GetComponent<Animator>();
+		_myAudioSource = GetComponent<AudioSource>();
 	}
 
 	public void Die()
 	{
 		_myAnimator.SetTrigger("Die");
+		_myAudioSource.PlayOneShot(breakingNoises[Random.Range(0, breakingNoises.Length)]);
 	}
 
 	public void DestroyThis()
